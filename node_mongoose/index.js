46e805e811b017ebdc5ab366f6e5337c641ba926
RootV <espiritusantiagovictor@gmail.com>
2020-06-14 14:14:35 -0500

    Cambios a ser confirmados: 	nuevo archivo:  node_mongoose/.gitignore 	nuevo archivo:  node_mongoose/index.js 	nuevo archivo:  node_mongoose/models/dishes.js 	nuevo archivo:  node_mongoose/package-lock.json 	nuevo archivo:  node_mongoose/package.json

---
+++ node_mongoose/index.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://172.17.0.2:27017/conFusion2';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log('Connected correctly to server ');
+
+    var newDish = Dishes({
+        name: 'Arrox Frito',
+        description: 'the best food'
+    });
+
+    newDish.save()
+        .then((dish) => {
+            console.log('New: ', dish)
+            return Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log('Dishes: ', dishes);
+
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log('ERROR:', err);
+        });
+});
