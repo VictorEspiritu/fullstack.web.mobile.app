5fe3f4b4d14f318c6c80f922223e959f3b064984
RootV <espiritusantiagovictor@gmail.com>
2020-06-16 07:51:45 -0500

    Add node_basic

---
+++ node_basic_examples/index.js
@@ -0,0 +1,24 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+}
+
+function solveRect(l, b){
+    console.log('Solving for rectangle: ', l, b);
+
+    if(l <=0 || b <= 0){
+        console.log('VALUES INCORRECT')
+    }else {
+        console.log('The area of rectangle is: ', rect.area(l,b));
+        console.log('The perimeter of rectangle is: ', rect.perimeter(l,b));
+
+    }
+}
+
+//Comments
+solveRect(2,3);
+solveRect(-2,3);
+solveRect(2,-3);
+solveRect(223,3655767);
+solveRect(0,3);
+
