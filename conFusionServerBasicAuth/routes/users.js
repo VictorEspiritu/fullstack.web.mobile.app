51010112e57642908c2a801d28f8c847673fc9ed
RootV <espiritusantiagovictor@gmail.com>
2020-06-21 12:16:02 -0500

    setting by ssh

--- conFusionServerBasicAuth/routes/users.js
+++ conFusionServerBasicAuth/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var bodyParser= require('body-parser');
+var User = require('../models/user');
+
 var router = express.Router();
+router.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
 
 module.exports = router;
